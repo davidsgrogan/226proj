8a5c7c720aecb9e8a8d81919c5772466129fb115
David Grogan <dgrogan@chromium.org>
2018-10-27 14:23:19 -0700

    sloppy notes to myself about getting more data

--- time_plot.R
+++ time_plot.R
@@ -15,3 +15,15 @@ ggplot(Ks, mapping = aes(x = yearID, y = KsPerAB)) +
   labs(title = "Strikeouts per At Bat over time") + 
   xlab("Year") +
   ylab("Proportion of At Bats that end in a strikeout")
+
+# We are trying to pick needles from haystacks if we are trying to determine
+# difference between regular season OPS and postseason OPS.  If that's what we
+# do, we need more data. We could just drop the column IBB, or for the missing
+# part of our dataset, impute it from other covars that are highly correlated
+# with it, if that's what is technically recommended in the chapter he posted
+# about imputation.
+
+# Or we can just plain old try to predict posteason OPS without comparing it to
+# regular season OPS, which is maybe preferable. But even then I'm interested
+# in getting more data by dropping or imputing columns that are NA close to
+# 1955.
